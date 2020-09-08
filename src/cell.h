19e729a12ca42b5917d83c5f2da7211c8c2d516c
hedykan <hedykan@outlook.com>
2020-09-08 10:57:22 +0800

    update Cell struct

--- src/cell.h
+++ src/cell.h
@@ -1,5 +1,12 @@
 #define SIZE 4
 
+struct Cell
+{
+    int status;
+    int status_size;
+    int status_group[8];
+};
+
 void print_cell(int *cell, int size);
 void calc_cell(int *cell, int size);
 int calc_cell_status(int *cell, int size, int local);
--- src/main.c
+++ src/main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include "cell.h"
-
 int main()
 {
-    int cell[SIZE] = {0, 0, 0, 0};
-    print_cell(cell, SIZE);
-    for(int i = 0; i < 10; i++)
+    struct Cell c[SIZE];
+    c[0].status = 0;
+    c[0].status_size = 8;
+    for(int i = 0; i < 8; i++)
     {
-        calc_cell(cell, SIZE);
-        print_cell(cell, SIZE);
-        printf("\n");
+        c[0].status_group[i] = 0;
     }
+    printf("%d,%d,%d\n", c[0].status, c[0].status_size, c[0].status_group[1]);
+
+    /* int cell[SIZE] = {0, 0, 0, 0}; */
+    /* print_cell(cell, SIZE); */
+    /* for(int i = 0; i < 10; i++) */
+    /* { */
+    /*     printf("\n"); */
+    /*     calc_cell(cell, SIZE); */
+    /*     print_cell(cell, SIZE); */
+    /* } */
+
     return 0;
 }
