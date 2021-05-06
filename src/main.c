044ffc1f7305357415ea0c00afd2846f218cd6d6
hedykan <hedykan@outlook.com>
2021-05-06 17:44:06 +0800

    update main

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@ void cell_init_test();
 int cell_scanf(struct Cell *cell_group);
 void cell_rule_train(struct CellRuleNode *rule_group, int rule_size, struct Cell *cell);
 void calc_rule_all(struct Cell *cell);
+void calc_rule_select(int rule_size, int status_size, int rule_count, int status_count, struct CellRuleNode **cell_rule_all, struct CellRuleNode **cell_rule_group);
 
 int main() // TODO 输入多个scanf
 {
@@ -102,4 +103,33 @@ void calc_rule_all(struct Cell *cell) {
         }
         printf("\n");
     }
+
+    struct CellRuleNode **cell_rule_group;
+    cell_rule_group = malloc(sizeof(struct CellRuleNode *) * status_size);
+    for(i = 0; i < status_size; i++) {
+        cell_rule_group[i] = malloc(sizeof(struct CellRuleNode) * rule_size);
+    }
+    calc_rule_select(rule_size, status_size, 0, 0, cell_rule_all, cell_rule_group);
+    /* for(i = 0; i < status_size; i++) { */
+    /*     for(j = 0; j < rule_size; j++) { */
+    /*         printf("%d,%d ", cell_rule_group[i][j].input_status, cell_rule_group[i][j].output_status); */
+    /*     } */
+    /*     printf("\n"); */
+    /* } */
+}
+
+// cell_rule_group 先分配好
+// cell_rule_group 是以
+void calc_rule_select(int rule_size, int status_size, int rule_count, int status_count, struct CellRuleNode **cell_rule_all, struct CellRuleNode **cell_rule_group) {
+    if(rule_count >= rule_size) {
+        printf("\n");
+        return;
+    }
+    printf("[%d,%d] ", rule_count, status_count);
+    // cell_rule_group[status_count][rule_count] = cell_rule_all[rule_count][status_count];
+    int i;
+    for(i = 0; i < status_size; i++) {
+        calc_rule_select(rule_size, status_size, ++rule_count, i, cell_rule_all, cell_rule_group);
+    }
+    return;
 }
