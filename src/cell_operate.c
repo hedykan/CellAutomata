533d483d1570583c49338199757b36a3ff8f167f
hedykan <hedykan@outlook.com>
2021-12-27 23:10:08 +0800

    update operate

--- src/cell_operate.c
+++ src/cell_operate.c
@@ -1,10 +1,12 @@
 #include "cell.h"
 
+void print_status(int status);
+
 // TODO 把他改的普适起来
 void cell_operate(struct Cell **cell_group, int cell_size, int change_index, int show_index)
 {
     int status;
-    while(1)
+    while (1)
     {
         // 在这一时刻瞬间改变传感器的输入值
         printf("please type an input:");
@@ -16,11 +18,32 @@ void cell_operate(struct Cell **cell_group, int cell_size, int change_index, int
         // 进行运算
         calc_cell_status(cell_group[show_index]);
         printf("emotion_cell's input: %d, emotion_cell's status: ", status);
-        print_cell_status(cell_group[show_index]);
+        // print_cell_status(cell_group[show_index]);
+        print_status(cell_group[show_index]->cell_status->status);
         printf("\n");
     }
 }
 
-void cell_status_change(struct Cell *cell, int status) {
+void cell_status_change(struct Cell *cell, int status)
+{
     cell->cell_status->status = status;
+}
+
+void print_status(int status)
+{
+    switch (status)
+    {
+    case 0:
+        printf("calm");
+        break;
+    case 1:
+        printf("happy");
+        break;
+    case 2:
+        printf("angry");
+        break;
+    case 3:
+        printf("sad");
+        break;
+    }
 }
